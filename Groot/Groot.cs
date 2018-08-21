85e2e0e2f2e8e8ce475b61f482ff07dadc79db82
Isara Naranirattisai <isaradream@gmail.com>
2018-08-21 18:18:03 +0700

    implement: GetDictFromCsv GetObjectFromCsv

---
+++ Groot/Groot.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace Groot
+ {
+     public class Groot
+     {
+
+         public static List<OUT> CsvReaderIters<OUT>(string filePath, Func<IEnumerable<(string, string)>, OUT> fn)
+         {
+             var csvlines = File.ReadAllLines(filePath);
+             var header = csvlines[0].Split(',').Select(s => s.Trim()).ToList();
+
+             return (from line in csvlines.Skip(1)
+                 let data = new Dictionary<string, string>()
+                 let enumRange = Enumerable.Range(start: 0, count: header.Count)
+                 select fn(line.Split(',')
+                     .ToList()
+                     .Zip(enumRange, (value, idx) => (header[idx], value)))).ToList();
+         }
+         
+         public static List<Dictionary<string, string>> GetDictFromCsv(string filePath)
+         {
+             return CsvReaderIters(filePath, xs => xs.ToDictionary(x => x.Item1.Trim(), x => x.Item2.Trim()));
+         }
+
+         public static List<T> GetObjectFromCsv<T>(string filePath)
+         {
+             Type type = typeof(T);
+             return GetDictFromCsv(filePath).Select(elem =>
+             {
+                 T tElem = (T) Activator.CreateInstance(typeof(T), new object[] { });
+                 foreach (KeyValuePair<string, string> x in elem)
+                 {
+                     PropertyInfo prop = type.GetProperty(x.Key);
+                     prop.SetValue(tElem, Convert.ChangeType(x.Value, prop.PropertyType), null);
+                 }
+                 return tElem;
+             }).ToList();
+             
+         }
+         
+         
+     }
+ }
--- Groot/Program.cs
+++ Groot/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.IO;
 
 namespace Groot
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            var path = Path.GetFullPath("input/input.csv");
+            var iamGroot = Groot.GetDictFromCsv(path);
+            Console.WriteLine(iamGroot[2]["booleanColumn"]);
+
+            var iamGrootObject = Groot.GetObjectFromCsv<Eiei>(path);
+            Console.WriteLine("eiei");
         }
+    }
+
+    public class Eiei
+    {
+        public int intColumn { get; set; }
+        public int int2Column { get; set; }
+        public string stringColumn { get; set; }
+        public bool booleanColumn { get; set; }
+        public decimal decimalColumn { get; set; }
+        public decimal decimal2Column { get; set; }
+        public long longNullableColumn { get; set; }
     }
 }
